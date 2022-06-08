f3a9fbc27f9e7466a2c7d3252f05952b5440d590
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2022-06-08 14:36:40 +0200

    add-player

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Phaser from "phaser";
 import backgroundImage from "./assets/background.jpg";
-import platform from "./assets/platform.png";
+import platform from "./assets/platform.png"; //32px height
+import mainCharacter from "./assets/mainCharacter.png";
+// 24 image, 576px
 
 class MyGame extends Phaser.Scene {
   constructor() {
@@ -10,6 +12,10 @@ class MyGame extends Phaser.Scene {
   preload() {
     this.load.image("backgroundImage", backgroundImage);
     this.load.image("ground", platform);
+    this.load.spritesheet("mainCharacter", mainCharacter, {
+      frameWidth: 24,
+      frameHeight: 24,
+    });
   }
 
   create() {
@@ -19,6 +25,12 @@ class MyGame extends Phaser.Scene {
     platforms.create(100, 600, "ground").setScale(0.5, 4).refreshBody();
     platforms.create(400, 600, "ground").setScale(0.5, 4).refreshBody();
     platforms.create(800, 600, "ground").setScale(0.5, 4).refreshBody();
+
+    const player = this.physics.add.sprite(100, 450, "mainCharacter");
+    player.setScale(2);
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.2);
+    this.physics.add.collider(player, platforms);
   }
 }
 
