e9b1c2957b97c4e55319ceb8306b2ed77bc7ede9
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2022-06-08 12:23:56 +0200

    Add ground

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import backgroundImage from "./assets/background.jpg";
+import platform from "./assets/platform.png";
 
 class MyGame extends Phaser.Scene {
   constructor() {
@@ -8,10 +9,13 @@ class MyGame extends Phaser.Scene {
 
   preload() {
     this.load.image("backgroundImage", backgroundImage);
+    this.load.image("ground", platform);
   }
 
   create() {
-    const logo = this.add.image(640, 360, "backgroundImage");
+    this.add.image(640, 360, "backgroundImage");
+    const platforms = this.physics.add.staticGroup();
+    platforms.create(640, 680, "ground").setScale(6, 4).refreshBody();
   }
 }
 
@@ -20,6 +24,13 @@ const config = {
   width: 1280,
   height: 720,
   scene: MyGame,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
