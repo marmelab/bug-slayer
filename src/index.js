1b2f7ff3f72d22d7dee094af97e171d37bc27d96
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2022-06-08 11:52:33 +0200

    add background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import backgroundImage from "./assets/background.jpg";
 
 class MyGame extends Phaser.Scene {
   constructor() {
@@ -7,20 +7,11 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("logo", logoImg);
+    this.load.image("backgroundImage", backgroundImage);
   }
 
   create() {
-    const logo = this.add.image(400, 150, "logo");
-
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1,
-    });
+    const logo = this.add.image(640, 360, "backgroundImage");
   }
 }
 
