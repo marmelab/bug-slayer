006d0df1d8a2c0271646847abef36b2b22c444be
Guillaume Pierson <guillaumep@marmelab.com>
2022-06-08 17:07:08 +0200

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import Phaser from "phaser";
-import backgroundImage from "./assets/background.jpg";
-import platform from "./assets/platform.png";
-import mainCharacter from "./assets/mainCharacter.png";
+import Phaser from 'phaser';
+import backgroundImage from './assets/background.jpg';
+import platform from './assets/platform.png';
+import mainCharacter from './assets/mainCharacter.png';
 
 class BugSlayer extends Phaser.Scene {
   #player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   #platforms!: Phaser.Physics.Arcade.StaticGroup;
 
   preload() {
-    this.load.image("backgroundImage", backgroundImage);
-    this.load.image("ground", platform);
-    this.load.spritesheet("mainCharacter", mainCharacter, {
+    this.load.image('backgroundImage', backgroundImage);
+    this.load.image('ground', platform);
+    this.load.spritesheet('mainCharacter', mainCharacter, {
       frameWidth: 24,
       frameHeight: 24,
     });
   }
 
   create() {
-    this.add.image(640, 360, "backgroundImage");
+    this.add.image(640, 360, 'backgroundImage');
     this.#platforms = this.physics.add.staticGroup();
-    this.#platforms.create(640, 680, "ground").setScale(6, 4).refreshBody();
-    this.#platforms.create(100, 600, "ground").setScale(0.5, 4).refreshBody();
-    this.#platforms.create(400, 600, "ground").setScale(0.5, 4).refreshBody();
-    this.#platforms.create(800, 600, "ground").setScale(0.5, 4).refreshBody();
+    this.#platforms.create(640, 680, 'ground').setScale(6, 4).refreshBody();
+    this.#platforms.create(100, 600, 'ground').setScale(0.5, 4).refreshBody();
+    this.#platforms.create(400, 600, 'ground').setScale(0.5, 4).refreshBody();
+    this.#platforms.create(800, 600, 'ground').setScale(0.5, 4).refreshBody();
 
-    this.#player = this.physics.add.sprite(100, 450, "mainCharacter");
+    this.#player = this.physics.add.sprite(100, 450, 'mainCharacter');
     this.#player.setScale(2);
     this.#player.setCollideWorldBounds(true);
     this.physics.add.collider(this.#player, this.#platforms);
@@ -53,7 +53,7 @@ const config = {
   height: 720,
   scene: BugSlayer,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: { y: 300 },
       debug: false,
@@ -61,4 +61,5 @@ const config = {
   },
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const game = new Phaser.Game(config);
